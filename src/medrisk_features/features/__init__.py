e15a56ef374d173ed8ce573a403bf1709089f197
Manda Surel <125080872+Manda404@users.noreply.github.com>
2026-01-13 15:46:55 +0100

    feat(features): add behavioral feature engineering

--- src/medrisk_features/features/__init__.py
+++ src/medrisk_features/features/__init__.py
@@ -2,10 +2,12 @@ from medrisk_features.features.demographics import DemographicsFeatureEngineer
 from medrisk_features.features.medical import MedicalFeatureEngineer
 from medrisk_features.features.clinical import ClinicalFeatureEngineer
 from medrisk_features.features.metabolic import MetabolicFeatureEngineer
+from medrisk_features.features.behavioral import BehavioralFeatureEngineer
 
 __all__ = [
     "DemographicsFeatureEngineer",
     "MedicalFeatureEngineer",
     "ClinicalFeatureEngineer",
     "MetabolicFeatureEngineer",
+    "BehavioralFeatureEngineer",
 ]
