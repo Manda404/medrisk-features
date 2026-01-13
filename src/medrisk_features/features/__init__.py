1e4e105eb1990642e35534c6d36cf84cdd534c2f
Manda Surel <125080872+Manda404@users.noreply.github.com>
2026-01-13 15:48:47 +0100

    feat(features): add lifestyle feature engineering

--- src/medrisk_features/features/__init__.py
+++ src/medrisk_features/features/__init__.py
@@ -3,6 +3,7 @@ from medrisk_features.features.medical import MedicalFeatureEngineer
 from medrisk_features.features.clinical import ClinicalFeatureEngineer
 from medrisk_features.features.metabolic import MetabolicFeatureEngineer
 from medrisk_features.features.behavioral import BehavioralFeatureEngineer
+from medrisk_features.features.lifestyle import LifestyleFeatureEngineer
 
 __all__ = [
     "DemographicsFeatureEngineer",
@@ -10,4 +11,5 @@ __all__ = [
     "ClinicalFeatureEngineer",
     "MetabolicFeatureEngineer",
     "BehavioralFeatureEngineer",
+    "LifestyleFeatureEngineer",
 ]
