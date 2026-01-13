42dca601f60432fea2bd538daca634f451499c43
Manda Surel <125080872+Manda404@users.noreply.github.com>
2026-01-13 15:43:23 +0100

    feat(features): add clinical interaction feature engineering

--- src/medrisk_features/features/__init__.py
+++ src/medrisk_features/features/__init__.py
@@ -1,7 +1,9 @@
 from medrisk_features.features.demographics import DemographicsFeatureEngineer
 from medrisk_features.features.medical import MedicalFeatureEngineer
+from medrisk_features.features.clinical import ClinicalFeatureEngineer
 
 __all__ = [
     "DemographicsFeatureEngineer",
     "MedicalFeatureEngineer",
+    "ClinicalFeatureEngineer",
 ]
